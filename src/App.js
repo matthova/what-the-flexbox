d898bb012cc3d9e868cbc2d4d3588b9bdf4c518f
matthova <matthova87@gmail.com>
2017-07-24 00:07:11 -0700

    let the flex begin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <div className="container">
+          <div className="box box1">1</div>
+          <div className="box box2">2</div>
+          <div className="box box3">3</div>
+          <div className="box box4">4</div>
+          <div className="box box5">5</div>
+          <div className="box box6">6</div>
+          <div className="box box7">7</div>
+          <div className="box box8">8</div>
+          <div className="box box9">9</div>
+          <div className="box box10">10</div>
+        </div>
       </div>
     );
   }
