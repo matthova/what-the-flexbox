c9e5265a7edb2daf456878eafbe17c362c54ac4c
matthova <matthova87@gmail.com>
2017-08-02 23:07:35 -0700

    partway through video 13

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <div className="box box1">1</div>
-          <div className="box box2">2</div>
-          <div className="box box3">3</div>
-          <div className="box box4">4</div>
-          <div className="box box5">5</div>
-          <div className="box box6">6</div>
-          <div className="box box7">7</div>
-          <div className="box box8">8</div>
-          <div className="box box9">9</div>
-          <div className="box box10">10</div>
-        </div>
+      <div className="App container">
+        <div className="box box1">One</div>
+        <div className="box box2">Twooo</div>
+        <div className="box box3">Three</div>
+        <div className="box box4">Four</div>
+        <div className="box box5">Five</div>
+        <div className="box box6">Six</div>
       </div>
     );
   }
 }
 
 export default App;
+
