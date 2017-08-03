9913912223b10a55773986ddce407ca9754e785b
matthova <matthova87@gmail.com>
2017-08-03 08:45:42 -0700

    Made a basic nav with breakpoints at 1000px and 500px

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <div className="box box1">One</div>
-        <div className="box box2">Twooo</div>
-        <div className="box box3">Three</div>
-        <div className="box box4">Four</div>
-        <div className="box box5">Five</div>
-        <div className="box box6">Six</div>
+      <div className="App">
+        <div className="wrapper">
+          <nav className="flex-nav">
+            <ul>
+              <li>
+                <a href="/">Item 01</a>
+              </li>
+              <li>
+                <a href="/">Item 02</a>
+              </li>
+              <li>
+                <a href="/">Item 03</a>
+              </li>
+              <li>
+                <a href="/">Item 04</a>
+              </li>
+              <li>
+                <a href="/">Item 05</a>
+              </li>
+              <li>
+                <a href="/">Item 06</a>
+              </li>
+              <li className="social">
+                <a href="http://twitter.com/matthova">∂ƒ©</a>
+              </li>
+              <li className="social">
+                <a href="http://facebook.com/matthewhovanec">å∫ß</a>
+              </li>
+              <li className="social">
+                <a href="http://github.com/matthova">œœœ</a>
+              </li>
+              <li className="social">
+                <a href="http://instagram.com/matthova">πåπ</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
