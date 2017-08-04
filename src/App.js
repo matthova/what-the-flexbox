a90e9dfaede0ed3d0351f86274da5ff9623b654f
matthova <matthova87@gmail.com>
2017-08-04 15:45:24 -0700

    Flexy nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      menuOpen: false
+    };
+
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({ menuOpen: !this.state.menuOpen });
+    return false;
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <div className="wrapper">
+          <div className="top chunk">
+            <h1>What The Flexbox?!</h1>
+          </div>
           <nav className="flex-nav">
-            <ul>
+            <a
+              className="menu"
+              style={{ cursor: "pointer" }}
+              onClick={this.toggleNav}
+            >
+              &#9776; Menu
+            </a>
+            <ul className={`toggleNav${this.state.menuOpen ? " open" : ""}`}>
               <li>
                 <a href="/">Item 01</a>
               </li>
@@ -40,6 +66,22 @@ class App extends Component {
               </li>
             </ul>
           </nav>
+          <section className="hero chunk">
+            <img alt="Hero Image" src="Fight Global Warming.jpg" />
+          </section>
+          <div className="chunk details">
+            <p>Cool stuff, yo</p>
+          </div>
+          <div className="chunk signup">
+            <form>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="email address" />
+              <input type="submit" value="Sign me up!" />
+            </form>
+          </div>
+          <div className="chunk">
+            <p>©Hovatown</p>
+          </div>
         </div>
       </div>
     );
