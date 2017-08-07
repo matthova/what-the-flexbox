a3e94c5aac4537069e1234c819456d6b498cba94
matthova <matthova87@gmail.com>
2017-08-06 23:26:23 -0700

    nav with arrows on the side and centered text

--- src/App.js
+++ src/App.js
@@ -2,86 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super();
-
-    this.state = {
-      menuOpen: false
-    };
-
-    this.toggleNav = this.toggleNav.bind(this);
-  }
-
-  toggleNav() {
-    this.setState({ menuOpen: !this.state.menuOpen });
-    return false;
-  }
-
   render() {
-    console.log(this.state);
     return (
       <div className="App">
         <div className="wrapper">
-          <div className="top chunk">
-            <h1>What The Flexbox?!</h1>
+          <div className="hero chunk">
+            <img alt="Fight Global Warming" src="Fight Global Warming.jpg" />
           </div>
-          <nav className="flex-nav">
-            <a
-              className="menu"
-              style={{ cursor: "pointer" }}
-              onClick={this.toggleNav}
-            >
-              &#9776; Menu
-            </a>
-            <ul className={`toggleNav${this.state.menuOpen ? " open" : ""}`}>
-              <li>
-                <a href="/">Item 01</a>
+          <nav className="slider-nav">
+            <ul>
+              <li className="arrow">
+                <a href="/">
+                  <span>←</span>
+                </a>
               </li>
               <li>
-                <a href="/">Item 02</a>
+                <a href="/">
+                  <span>Item 01. oh yeah</span>
+                </a>
               </li>
               <li>
-                <a href="/">Item 03</a>
+                <a href="/">
+                  <span>Item 02</span>
+                </a>
               </li>
               <li>
-                <a href="/">Item 04</a>
+                <a href="/">
+                  <span>Item 03</span>
+                </a>
               </li>
-              <li>
-                <a href="/">Item 05</a>
-              </li>
-              <li>
-                <a href="/">Item 06</a>
-              </li>
-              <li className="social">
-                <a href="http://twitter.com/matthova">∂ƒ©</a>
-              </li>
-              <li className="social">
-                <a href="http://facebook.com/matthewhovanec">å∫ß</a>
-              </li>
-              <li className="social">
-                <a href="http://github.com/matthova">œœœ</a>
-              </li>
-              <li className="social">
-                <a href="http://instagram.com/matthova">πåπ</a>
+              <li className="arrow">
+                <a href="/">
+                  <span>→</span>
+                </a>
               </li>
             </ul>
           </nav>
-          <section className="hero chunk">
-            <img alt="Hero Image" src="Fight Global Warming.jpg" />
-          </section>
-          <div className="chunk details">
-            <p>Cool stuff, yo</p>
-          </div>
-          <div className="chunk signup">
-            <form>
-              <input type="text" placeholder="Name" />
-              <input type="text" placeholder="email address" />
-              <input type="submit" value="Sign me up!" />
-            </form>
-          </div>
-          <div className="chunk">
-            <p>©Hovatown</p>
-          </div>
         </div>
       </div>
     );
