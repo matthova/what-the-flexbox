4a1cd5aad708f3f189516601428f860a6c542bd0
matthova <matthova87@gmail.com>
2017-08-07 00:00:46 -0700

    Made a 3-column pricing grid

--- src/App.js
+++ src/App.js
@@ -5,39 +5,52 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="wrapper">
-          <div className="hero chunk">
-            <img alt="Fight Global Warming" src="Fight Global Warming.jpg" />
-          </div>
-          <nav className="slider-nav">
-            <ul>
-              <li className="arrow">
-                <a href="/">
-                  <span>←</span>
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  <span>Item 01. oh yeah</span>
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  <span>Item 02</span>
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  <span>Item 03</span>
-                </a>
-              </li>
-              <li className="arrow">
-                <a href="/">
-                  <span>→</span>
-                </a>
-              </li>
+        <div className="pricing-grid">
+          <div className="plan plan1">
+            <h2>Cat</h2>
+            <p>Common, yet regarded by many as the worst house pet.</p>
+            <ul className="features">
+              <li>Scratches everything</li>
+              <li>Easily lost for days</li>
+              <li>Kind of a bummer</li>
             </ul>
-          </nav>
+
+            <p className="price">free</p>
+
+            <a href="#" className="cta">
+              Really?1?
+            </a>
+          </div>
+          <div className="plan plan2">
+            <h2>Dog</h2>
+            <p>
+              Loving, gentle, and caring. Dogs are the best housepet to have and
+              will increase happiness ten fold.
+            </p>
+            <ul class="features">
+              <li>Super Fun</li>
+              <li>Friends love them</li>
+              <li>Plays games</li>
+              <li>Not a cat</li>
+            </ul>
+            <p className="price">$90</p>
+            <a href="#" className="cta">
+              Best Deal
+            </a>
+          </div>
+          <div className="plan plan3">
+            <h2>Fish</h2>
+            <p>Cheap and easy</p>
+            <ul className="features">
+              <li>Eats flakes</li>
+              <li>Cop out</li>
+              <li>Replacable</li>
+            </ul>
+            <p className="price">$3</p>
+            <a href="#" className="cta">
+              Fsh
+            </a>
+          </div>
         </div>
       </div>
     );
